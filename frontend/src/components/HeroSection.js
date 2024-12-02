ff202b82e2b1b47f981b0a6cc889ba02deb34dcc
OumaimaRouis <oumaima.rouis01@gmail.com>
2024-12-02 10:27:02 +0100

    some changes4

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../App.css';
 import { Button } from './Button';
 import './HeroSection.css';
+import {useLocation } from 'react-router-dom';
+
 
 function HeroSection() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const location = useLocation();
+
+
+  useEffect(() => {
+    const isAuth = localStorage.getItem('isAuth') === 'true';
+    setIsAuthenticated(isAuth);
+  }, [location]);
+
   return (
     <div className='hero-container'>
       {/*<video src='/videos/video-1.mp4' autoPlay loop muted />*/} 
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
+        {!isAuthenticated && (
+
+        
         <Button
           className='btns'
           buttonStyle='btn--outline'
@@ -16,6 +30,7 @@ function HeroSection() {
         >
           GET STARTED
         </Button>
+        )}
         <Button
           className='btns'
           buttonStyle='btn--primary'
